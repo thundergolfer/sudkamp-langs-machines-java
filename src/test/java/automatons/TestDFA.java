8f81437e98a236260ae891086bcd27ef5577f072
Jonathon Ian Belotti <s3544121@student.rmit.edu.au>
2016-07-21 22:21:39 +1000

    Adding a couple of unit tests for DFA

--- src/test/java/automatons/TestDFA.java
+++ src/test/java/automatons/TestDFA.java
@@ -2,13 +2,33 @@ package automatons;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class TestDFA {
+import utility.InputUtil;
 
+public class TestDFA {
+	
+	DFA<String> dfaOne;
+
+	@Before
+	public void setUpGrammars() {
+		dfaOne = StringFAMaker.buildDFAWithEquivStates();
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+
+	
+	@Test
+	public void testRun() {
+		String word = "abbbbbb"; // this is known to be valid for the following dfa
+		List<String> input = InputUtil.wordToList(word);
+		assertTrue( dfaOne.run(input) );
 	}
 	
 	@Test
@@ -33,6 +53,14 @@ public class TestDFA {
 	@Test
 	public void testDist() {
 		
+	}
+	
+	@Test 
+	public void testGetArcWithSymbol() {
+		State s = dfaOne.startState;
+		FATransition<String> t = dfaOne.getArcWithSymbol("a", s);
+		assertNotNull( t );
+		assertEquals( t.symbol, "a");
 	}
 
 }
